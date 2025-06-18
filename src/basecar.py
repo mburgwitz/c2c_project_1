3876acbde411c711a60d6c791d8f0a6f3150bd28
Patrick384-lab <dia2233111@web.de>
2025-06-18 09:45:44 +0200

    Doc-Strings in BaseCar kommentiert

--- src/basecar.py
+++ src/basecar.py
@@ -1,15 +1,26 @@
 from basisklassen import FrontWheels, BackWheels
 import time
-class BaseCar:
 
+class BaseCar:
+    '''
+    BaseCar Klasse ist die Klasse mit den Grundfahrfunktionen des Fahrzeuges.
+
+    Methoden:
+        drive - gibt Geschwindigkeit, Fahrrichtung (durch das Vorzeichen der Geschwindigkeit) und Lenkwinkel (Fahrmodus)
+        stop - setzt speed zu 0
+    
+    Begrenzungswerte:
+        Der Lenkwinkel ist nur im Wertebereich 45 bis 135 (90 ist geradeaus)
+        Die Geschwindigkeit ist im Wertebereich -100 bis 100 (negative Geschwindigkeiten erzeugen eine Rückwärtsfahrt)
+        
+    '''
     MAX_STEERING_ANGLE = 135
     MIN_STEERING_ANGLE = 45
 
     MAX_SPEED = 100
     MIN_SPEED = -100
 
-    def __init__(self, steering_angle: float = 0.0, speed: float = 0.0, direction: int = 0):
-    
+    def __init__(self, steering_angle: int = 90.0, speed: int = 0.0, direction: int = 0):
         self.__steering_angle = steering_angle
         self.__speed = speed
         self.__direction = direction  
@@ -17,26 +28,26 @@ class BaseCar:
         self.__bw = BackWheels()      
 
     @property
-    def steering_angle(self):
+    def __steering_angle(self):
         return self.__steering_angle
     
-    @steering_angle.setter
-    def steering_angle(self, new_angle: float):
+    @__steering_angle.setter
+    def __steering_angle(self, new_angle: int):
         self.__steering_angle = self.__checkSteeringAngle(new_angle)
     
     @property
-    def speed(self):
+    def __speed(self):
         return self.__speed
     
-    @speed.setter
-    def speed(self, speed: int):
+    @__speed.setter
+    def __speed(self, speed: int):
         self.__speed = self.__checkSpeed(speed)
     
     @property
-    def direction(self):
+    def __direction(self):
         return self.__direction
     
-    def __checkSteeringAngle(self, angle: float) -> float:
+    def __checkSteeringAngle(self, angle: int) -> int:
         if angle < self.MIN_STEERING_ANGLE: 
             return self.MIN_STEERING_ANGLE 
         elif angle > self.MAX_STEERING_ANGLE:
@@ -50,7 +61,19 @@ class BaseCar:
             return self.MAX_SPEED
         return speed
     
-    def drive(self, speed: int = None, angle: float = None):
+    def drive(self, speed: int = None, angle: int = None):
+        '''
+        Die drive-Methode setzt mittels Parameter speed die Geschwindigkeit und die Fahrtrichtung (über Vorzeichen) und 
+        mittels Parameter angle wird der Lenkwinkel gesetzt.
+
+        Parameter:
+            speed - optional, wenn nicht angegeben wird der alte Wert übernommen und ist begrenzt auf den Wertebereich (-100 bis 100)
+            angle - optional, wenn nicht angegeben wird der alte Wert übernommen und ist begrenzt auf den Wertebereich (45 Volleinschlag links | 90 geradeaus | 135 Volleinschlag rechts)
+
+        Return - Value:
+            gibt keinen Wert zurück
+
+        '''
         if angle is not None:
             self.steering_angle = angle
         if speed is not None:
@@ -69,6 +92,16 @@ class BaseCar:
         self.__bw.speed = abs(self.speed)        
 
     def stop(self):
+        '''
+        Die stop-Methode setzt die Geschwindigkeit auf 0 
+
+        Parameter:
+            keine Parameter
+
+        Return-Value:
+            gibt keinen Wert zurück
+
+        '''
         self.__bw.stop()
         self.__direction = 0
 
